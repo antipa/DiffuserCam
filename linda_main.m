ccd0884d7953f56d25d151b98c5158b72c06fd35
Linda Liu <fanglin_liu@berkeley.edu>
2018-08-03 17:00:46 -0700

    Delete linda_main.m

--- linda_main.m
+++
@@ -1,6 +0,0 @@
-xhat_out = uint16(65535*(xhat/max(xhat(:))));
-for layer=7:16
-    imout = xhat_out(:,:,layer);
-    imwrite(imout,['\\WALLER-PHANTOM\PhantomData\Diffusers''nstuff\Big_DiffuserMicroscope\180606_worm\1worm',...
-        '\-20\-20\processed\img_000000008_layer',num2str(layer),'.tif'])
-end
